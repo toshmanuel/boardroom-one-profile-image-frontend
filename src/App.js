d9f091fd68594b66164a3d126b400036d54c489c
toshmanuel <oladimejiomotosho5@gmail.com>
2021-08-19 11:39:50 +0100

    boardroom one frontend page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Layout, Container, BoxUpload, ImagePreview } from "./style";
+import FolderIcon from "./assets/folder_icon_transparent.png";
+import CloseIcon from "./assets/CloseIcon.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  // const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("")
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [typeFile, setTypeFile] = useState("");
 
+  
+
+  const imageHandler = async(event) => {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+
+    let response  = await fetch(`https://boardroom-one-image.herokuapp.com/profile-photo/add-image`,
+    {
+      method: "POST",
+      body: formData,
+      // headers:{
+      //   "Accept": "multipart/form-data",
+      // }
+    })
+
+    if (response.ok){
+      await response.json()
+      .then(result => {
+        console.log(result)
+        setImageUrl(result.secure_url)
+      })
+      .then(()=> setIsUploaded(true))
+    }
+  }
+  
+
+  return (
+    <Layout>
+      <Container>
+        <h2>Upload your image</h2>
+        <p>Upload with preview 😁</p>
+
+        <BoxUpload>
+          <div className="image-upload">
+            {!isUploaded ? (
+              <>
+                <label htmlFor="upload-input">
+                  <img
+                    src={FolderIcon}
+                    draggable={"false"}
+                    alt="placeholder"
+                    style={{ width: 100, height: 100 }}
+                  />
+                  <p style={{ color: "#444" }}>Click to upload image</p>
+                </label>
+
+                <input
+                  id="upload-input"
+                  type="file"
+                  accept=".jpg,.jpeg,.gif,.png"
+                  onChange={imageHandler}
+                />
+              </>
+            ) : (
+              <ImagePreview>
+                <img
+                  className="close-icon"
+                  src={CloseIcon}
+                  alt="CloseIcon"
+                  onClick={() => {
+                    setIsUploaded(false);
+                    setImageUrl(null);
+                  }}
+                />
+                {typeFile.includes("video") ? (
+                  <video
+                    id="uploaded-image"
+                    src={imageUrl}
+                    draggable={false}
+                    controls
+                    autoPlay
+                    alt="uploaded-img"
+                  />
+                ) : (
+                  <img
+                    id="uploaded-image"
+                    src={imageUrl}
+                    draggable={false}
+                    alt="uploaded-img"
+                  />
+                )}
+              </ImagePreview>
+            )}
+          </div>
+        </BoxUpload>
+
+      </Container>
+    </Layout>
+  );
+
+}
 export default App;
